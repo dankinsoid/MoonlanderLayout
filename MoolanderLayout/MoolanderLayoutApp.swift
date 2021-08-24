07579d650aba58462f54c03498f51acbf3f27a06
dankinsoid <30962149+dankinsoid@users.noreply.github.com>
2021-08-24 10:31:44 +0300

    Initial Commit

---
+++ MoolanderLayout/MoolanderLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoolanderLayoutApp.swift
+//  MoolanderLayout
+//
+//  Created by Данил Войдилов on 24.08.2021.
+//
+
+import SwiftUI
+
+@main
+struct MoolanderLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
